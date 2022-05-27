f85d8b18fc3bd5b7c09088e796d17f0248412146
shashank725 <shashank.nov725@gmail.com>
2022-05-28 01:41:46 +0530

    [+] Models.py

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Customer, Product, Order
 
 # Register your models here.
+
+@admin.register(Customer)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'phone', 'email', 'date_created']
+
+admin.site.register(Product)
+admin.site.register(Order)
