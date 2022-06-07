aaea3bd5beca0b46354da8a846fcf31540733c8a
shashank725 <shashank.nov725@gmail.com>
2022-06-08 02:04:36 +0530

    Rendering Data to Templates

--- customer/urls.py
+++ customer/urls.py
@@ -5,5 +5,5 @@ app_name='customer'
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
-    path('customer/', views.customer, name='customer'),
+    path('customer/<str:pk>', views.customer, name='customer'),  #https://docs.djangoproject.com/en/4.0/topics/http/urls/
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,13 +1,28 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from customer.models import Customer, Order, Product
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'customer/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    context = {'orders':orders, 'customers':customers, 'total_customers':total_customers, 'total_orders':total_orders, 'delivered':delivered, 'pending':pending}
+    return render(request, 'customer/dashboard.html', context)
 
 def products(request):
-    return render(request, 'customer/products.html')
+    products = Product.objects.all()
+    return render(request, 'customer/products.html', {'products': products})
 
-def customer(request):
-    return render(request, 'customer/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    total_order = orders.count()
+    context = {'customer':customer, 'orders':orders, 'total_order':total_order}
+    return render(request, 'customer/customer.html', context)
